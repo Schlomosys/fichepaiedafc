a00e3112b78e2f9298c138661ef255249de5ca51
Schallom Awolou <64165396+Schlomosys@users.noreply.github.com>
2021-09-12 21:01:32 +0200

    Update SendBulkQueueEmail.php

--- app/Jobs/SendBulkQueueEmail.php
+++ app/Jobs/SendBulkQueueEmail.php
@@ -47,7 +47,7 @@ class SendBulkQueueEmail implements ShouldQueue
          {
         //$user_details = Users::where('id',$invoice->user_id)->first();
            $html = '';
-           $signat= $user =User::findOrFail(3);
+           $signat= $user =User::findOrFail(2);
            $view = view('fiche_paie')->with(compact('fichepaie', 'signat'));
            $html .= $view->render();
            #set_time_limit(0);
